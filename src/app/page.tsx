ccd2b81888860a44ea29971c8fdcbe4aa20b978f
IrronWilliams <irron.williams@gmail.com>
2025-02-24 01:41:05 -0500

    adding About and Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { About } from '@/components/About'
+import { Hero } from '@/components/Hero'
+
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Testing Connection!
-    </h1>
+    <>
+      <Hero />
+      <About />
+    
+    </>
   )
 }
